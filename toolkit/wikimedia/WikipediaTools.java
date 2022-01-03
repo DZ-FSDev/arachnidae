9ae0cd591f4da0f35cb1e6c55b5278e4274dbc17
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2022-01-03 12:44:02 -0600

    Create WikipediaTools.java

---
+++ toolkit/wikimedia/WikipediaTools.java
@@ -0,0 +1,87 @@
+/*  Original Licensing Copyright
+ * 
+ *  Various methods to access data from Wikipedia.
+ *  Copyright (C) 2022  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.dz_fs_dev.arachnidae.toolkit.wikimedia;
+
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.dz_fs_dev.common.counters.PerMinuteExponentialRateCounter;
+import com.dz_fs_dev.common.net.NetTools;
+
+/**
+ * Contains various methods to access data from Wikipedia. Keeps track of
+ * request rates to prevent abuse filtering. When the internal rate limit is
+ * hit, the default behavior is to hang the executing thread to adhere to the
+ * specified limit. This behavior can be modified with
+ * {@link WikipediaTools#setSleepThrottling} to cause an exception to be
+ * thrown instead.
+ * 
+ * @author DZ_FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public final class WikipediaTools {
+	private WikipediaTools() {}
+	
+	/**
+	 * The default read rate limit per minute to adhere to when no limit was
+	 * provided by Wikipedia's API on application initialization. 
+	 */
+	private static final int DEFAULT_READ_RATELIMIT = 200;
+	private static final int EMA_PERIOD = 6;
+	
+	private static boolean sleepThrottling = true;
+	
+	//TODO https://en.wikipedia.org/w/api.php?action=query&meta=userinfo&uiprop=ratelimits
+	private static PerMinuteExponentialRateCounter requestRateCounter =
+			new PerMinuteExponentialRateCounter(WikipediaTools.EMA_PERIOD);
+
+	/**
+	 * 
+	 * 
+	 * @param query
+	 * @return
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	public static String[] getSuggestions(String query) throws JSONException, IOException {
+		if(sleepThrottling && requestRateCounter.poll() > WikipediaTools.DEFAULT_READ_RATELIMIT) {
+			//TODO
+		}
+		requestRateCounter.tick();
+		JSONArray jarr = NetTools.tryReadJSONArrayFromUrl(
+				String.format("https://en.wikipedia.org/w/api.php?action=opensearch&search=%s", query)).getJSONArray(1);
+		String[] ret = new String[jarr.length()];
+		
+		for(int i = 0; i < ret.length; i++) {
+			ret[i] = jarr.getString(i);
+		}
+		
+		return ret;
+	}
+	
+	/**
+	 * 
+	 * @param sleepThrottling
+	 */
+	public static void setSleepThrottling(boolean sleepThrottling) {
+		WikipediaTools.sleepThrottling = sleepThrottling;
+	}
+}
